8a7c6fd3b0fccfd8e671ba92f837d7346856b7c1
MJ Kryjak <mjk557@fusion131.its.york.ac.uk>
2022-06-08 15:05:38 +0100

    Feature: sdrun.py now supports --restart and --append

--- sdrun.py
+++ sdrun.py
@@ -7,6 +7,9 @@ import argparse
 
 parser = argparse.ArgumentParser(description = "Run case")
 parser.add_argument("case", type=str, help = "Case to run")
+parser.add_argument("--restart", action="store_true", help = "Restart?")
+parser.add_argument("--append", action="store_true", help = "Append?")
+
 args = parser.parse_args()
 
 cwd = os.getcwd()
@@ -18,6 +21,14 @@ sd1dpath = "/ssd_scratch/SD1D-mk/build"
 os.chdir(sd1dpath)
 print(casepath)
 
-subprocess.call(["screen", "-dmS", args.case, "./sd1d", "-d", casepath])
+if args.restart and args.append:
+    subprocess.call(["screen", "-dmS", args.case, "./sd1d", "-d", casepath, "restart", "append"])
+elif args.restart and not args.append:
+    subprocess.call(["screen", "-dmS", args.case, "./sd1d", "-d", casepath, "restart"])
+elif args.append and not args.restart:
+    subprocess.call(["screen", "-dmS", args.case, "./sd1d", "-d", casepath, "append"])
+else:
+    subprocess.call(["screen", "-dmS", args.case, "./sd1d", "-d", casepath])
 
+print(f"Restart: {args.restart}, Append: {args.append}")
 print("-> Case {} running".format(args.case))
