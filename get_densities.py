358fa9bef3ab9c2948f2abe70318fb8bf89fb776
MJ Kryjak <mjk557@fusion131.its.york.ac.uk>
2022-06-21 11:31:05 +0100

    Fix get densities when nesource 0

--- get_densities.py
+++ get_densities.py
@@ -2,6 +2,7 @@
 from boutdata.collect import collect
 from boutdata.data import BoutData
 import argparse
+import numpy as np
 import fnmatch
 import os
 
@@ -14,9 +15,14 @@ def get_densities(casepath):
     data = BoutData(casepath)
     Nnorm = data["outputs"]["Nnorm"]
     Tnorm = data["outputs"]["Tnorm"]
+    Omega_ci = data["outputs"]["Omega_ci"]
     
     Nn = data["outputs"]["Nn"][-1, 0, :, 0] * Nnorm
     Ne = data["outputs"]["Ne"][-1, 0, :, 0] * Nnorm
+    try:
+        NeSource = data["outputs"]["NeSource"][-1,0,:] * Nnorm * Omega_ci
+    except:
+        NeSource = np.zeros_like(Ne)
     dy = data["outputs"]["dy"][0, :]
     J = data["outputs"]["J"][0, :]
     
@@ -25,12 +31,13 @@ def get_densities(casepath):
     Ne_avg = sum(Ne * dV) / sum(dy) 
     Nn_avg = sum(Nn * dV) / sum(dy)
     Ntot_avg = Ne_avg + Nn_avg
+    NeSource_int = sum(NeSource * dV) # in s-1
     Nu = Ne[0]
 
     print(f"\n>>> {casepath}")
     print(f"Ne: {Ne_avg:.3E} || Nn: {Nn_avg:.3E} || Total: {Ntot_avg:.3E}")
     print(f"-------------------------------------------------------------")
-    print(f"Nu: {Nu:.3E}\n")
+    print(f"Nu: {Nu:.3E} || NeSource integral: {NeSource_int:.3E} [s-1]\n")
 
 
 #------------------------------------------------------------
@@ -54,7 +61,7 @@ if __name__ == "__main__":
     # If not a file, and if fnmatch found it, and if it has an input file... collect
     to_read = []
     for folder in os.listdir(cwd):
-        if "." not in folder and fnmatch.fnmatch(folder, fnsearch) and "BOUT.dmp.0.nc" in os.listdir(cwd+sep+folder):
+        if fnmatch.fnmatch(folder, fnsearch) and "BOUT.dmp.0.nc" in os.listdir(cwd+sep+folder):
             to_read.append(folder)
             
     for case in to_read:
