08903d495ef9fcd5b6ce63c819cf092495f8c12c
mikekryjak <mikekryjak@gmail.com>
2022-08-02 11:38:19 +0100

    Add rename rename_multiple

--- sdrun.py
+++ sdrun.py
@@ -9,6 +9,8 @@ parser = argparse.ArgumentParser(description = "Run case")
 parser.add_argument("case", type=str, help = "Case to run")
 parser.add_argument("--restart", action="store_true", help = "Restart?")
 parser.add_argument("--append", action="store_true", help = "Append?")
+parser.add_argument("--b2", action="store_true", help = "Use 2nd build?")
+
 
 args = parser.parse_args()
 
@@ -18,17 +20,22 @@ sep = os.path.sep
 casepath = cwd + sep + args.case
 sd1dpath = "/ssd_scratch/SD1D-mk/"
 
+if args.b2 == True:
+    build_folder = "build2"
+else:
+    build_folder = "build"
+
 os.chdir(sd1dpath)
 print(casepath)
 
 if args.restart and args.append:
-    subprocess.call(["screen", "-dmS", args.case, "build/sd1d", "-d", casepath, "restart", "append"])
+    subprocess.call(["screen", "-dmS", args.case, f"{build_folder}/sd1d", "-d", casepath, "restart", "append"])
 elif args.restart and not args.append:
-    subprocess.call(["screen", "-dmS", args.case, "build/sd1d", "-d", casepath, "restart"])
+    subprocess.call(["screen", "-dmS", args.case, f"{build_folder}/sd1d", "-d", casepath, "restart"])
 elif args.append and not args.restart:
-    subprocess.call(["screen", "-dmS", args.case, "build/sd1d", "-d", casepath, "append"])
+    subprocess.call(["screen", "-dmS", args.case, f"{build_folder}/sd1d", "-d", casepath, "append"])
 else:
-    subprocess.call(["screen", "-dmS", args.case, "build/sd1d", "-d", casepath])
+    subprocess.call(["screen", "-dmS", args.case, f"{build_folder}/sd1d", "-d", casepath])
 
 if args.restart:
     print("---> RESTARTING")
