13af82262ce05bdc63838b391fd369f6ffa65855
Mike Kryjak <mikekryjak@gmail.com>
2023-03-15 14:25:53 +0000

    Big improvements to viewer2d, lineplot

--- code_comparison/code_comparison.py
+++ code_comparison/code_comparison.py
@@ -174,48 +174,74 @@ class SOLPSdata:
         self.regions["imp"] = pd.concat(self.params["imp"].values(), axis = 1)
         self.regions["outer_lower"] = pd.concat(self.params["outer_lower"].values(), axis = 1)
         
-        self.process_omp()
-        self.process_imp()
-        self.process_target()
+        self.translate_names()
         
         
-    def process_omp(self):
-        self.regions["omp"] = self.regions["omp"].rename(columns = {
-            "ti3da":"Td+",
-            "te3da":"Te",
-            "ne3da":"Ne",
-            "dab23da":"ND", # As in SOLPS
-            "dmb23da":"ND2" # As in SOLPS
-        }
-        )
+    def translate_names(self):
+        # TODO Optimise this
         
-        self.regions["omp"]["Nd"] = self.regions["omp"]["ND"] + self.regions["omp"]["ND2"]*2 # Combine atomic and molecular data to compare with Hermes-3
+        for region in self.regions.keys():
+            df = self.regions[region]
+            
+            name_map = {
+                "ti3":"Td+",
+                "te3":"Te",
+                "ne3":"Ne",
+                "dnb23":"Nd",
+                "tab23":"Td"
+            }
+            
+            # If key in name_map, rename accordingly
+            for key in name_map.keys():
+                new_columns = []
+                for col in df.columns:
+                    if re.search(f"^{key}..", col):
+                        new_columns.append(name_map[key])
+                        print(f"{name_map[key]} found: {col}")
+                    else:
+                        new_columns.append(col)
+                df.columns = new_columns
+        
+    # def process_omp(self):
+    #     self.regions["omp"] = self.regions["omp"].rename(columns = {
+    #         "ti3da":"Td+",
+    #         "te3da":"Te",
+    #         "ne3da":"Ne",
+    #         "dnb23di":"Nd",
+    #         "dab23da":"ND", # As in SOLPS
+    #         "dmb23da":"ND2" # As in SOLPS
+    #     }
+    #     )
+        
+    #     self.regions["omp"]["Nd"] = self.regions["omp"]["ND"] + self.regions["omp"]["ND2"]*2 # Combine atomic and molecular data to compare with Hermes-3
         
     
-    def process_imp(self):
-        self.regions["imp"] = self.regions["imp"].rename(columns = {
-            "ti3di":"Td+",
-            "te3di":"Te",
-            "ne3di":"Ne",
-            "dab23di":"ND", # As in SOLPS
-            "dmb23di":"ND2" # As in SOLPS
-        }
-        )
+    # def process_imp(self):
+    #     self.regions["imp"] = self.regions["imp"].rename(columns = {
+    #         "ti3di":"Td+",
+    #         "te3di":"Te",
+    #         "ne3di":"Ne",
+    #         "dnb23di":"Nd",
+    #         "dab23di":"ND", # As in SOLPS
+    #         "dmb23di":"ND2" # As in SOLPS
+    #     }
+    #     )
         
-        self.regions["imp"]["Nd"] = self.regions["imp"]["ND"] + self.regions["imp"]["ND2"]*2 # Combine atomic and molecular data to compare with Hermes-3
+    #     # self.regions["imp"]["Nd"] = self.regions["imp"]["ND"] + self.regions["imp"]["ND2"]*2 # Combine atomic and molecular data to compare with Hermes-3
                 
             
-    def process_target(self):
-        self.regions["outer_lower"] = self.regions["outer_lower"].rename(columns = {
-            "ti3dr":"Td+",
-            "te3dr":"Te",
-            "ne3dr":"Ne",
-            "dab23dr":"ND", # As in SOLPS
-            "dmb23dr":"ND2" # As in SOLPS
-        }
-        )
+    # def process_target(self):
+    #     self.regions["outer_lower"] = self.regions["outer_lower"].rename(columns = {
+    #         "ti3dr":"Td+",
+    #         "te3dr":"Te",
+    #         "ne3dr":"Ne",
+    #         "dnb23di":"Nd",
+    #         "dab23dr":"ND", # As in SOLPS
+    #         "dmb23dr":"ND2" # As in SOLPS
+    #     }
+    #     )
         
-        self.regions["outer_lower"]["Nd"] = self.regions["outer_lower"]["ND"] + self.regions["outer_lower"]["ND2"]*2 # Combine atomic and molecular data to compare with Hermes-3
+    #     self.regions["outer_lower"]["Nd"] = self.regions["outer_lower"]["ND"] + self.regions["outer_lower"]["ND2"]*2 # Combine atomic and molecular data to compare with Hermes-3
 
                 
 class Hermesdata:
@@ -236,7 +262,7 @@ class Hermesdata:
     def compile_results(self, dataset):
         self.dataset = dataset
 
-        params = ["Td+", "Te", "Ne", "Nd", "Sd+_iz"]
+        params = ["Td+", "Td", "Te", "Ne", "Nd", "Sd+_iz"]
         x = []
         for param in params:
             data = self.dataset[param]
@@ -268,7 +294,171 @@ class viewer_2d():
                  cases,
                  vmin = None,
                  vmax = None,
+                 xlim = (None,None),
+                 ylim = (None,None),
                  logscale = True,
+                 dpi = 120,
+                 cmap = "Spectral_r"):
+        
+
+        self.param = param
+        self.cases = cases
+        num_cases = len(cases.keys())
+        self.max, self.min = self.find_ranges()
+
+        # Find ranges if not provided
+        if vmin == None:
+            vmin = self.min
+        if vmax == None:
+            vmax = self.max
+
+        fig = plt.figure(dpi=dpi)
+        fig.set_figheight(6)
+        fig.set_figwidth(num_cases*4)
+
+        # Plot grid
+        gs0a = mpl.gridspec.GridSpec(
+                                        ncols=num_cases+1, nrows=2,
+                                        width_ratios = [1]*num_cases + [0.1], # cbar, empty space, control,
+                                        height_ratios = [0.95, 0.05],
+                                        wspace = 0.05
+                                        )
+    
+        
+
+        axes = [None]*(num_cases+3)
+
+
+        for i, casename in enumerate(cases.keys()):
+            
+            # All plots after the first one share x and y axes
+            if i == 0:
+                axes[i] = fig.add_subplot(gs0a[i])
+            else:
+                axes[i] = fig.add_subplot(gs0a[i], sharex=axes[0], sharey=axes[0])
+            
+            model = cases[casename]
+            
+            if model["code"] == "hermes":
+                model["ds"][param].bout.polygon(ax = axes[i], add_colorbar = False, logscale = logscale, separatrix = True, cmap = cmap, vmin = vmin, vmax = vmax, antialias = False)
+                axes[i].set_title(f"Hermes-3\{casename}")
+                
+            elif model["code"] == "solps":
+                
+                
+                try:
+                    solps_name = name_parser(param,"solps")
+                    
+                except:
+                    print(f"Parameter {param} not found in SOLPS")
+                    solps_name = None
+                    
+                if solps_name != None:
+                    plot_2d(fig, axes[i], where = model["path"], what = name_parser(param,"solps"), cmap = "Spectral_r", scale = ("log" if logscale is True else "linear"), vmin = vmin, vmax = vmax, plot_cbar = False)
+                    axes[i].set_title(f"SOLPS\{casename}")
+                
+                else:
+                    axes[i].set_title(f"SOLPS\{casename}: {param} not found")
+                    
+                    
+                # plot_2d(fig, axes[i], where = model["path"], what = name_parser(param,"solps"), cmap = "Spectral_r", scale = ("log" if logscale is True else "linear"), vmin = vmin, vmax = vmax, plot_cbar = False)
+                # axes[i].set_title(f"SOLPS\{casename}")
+                
+            if xlim != (None, None):
+                axes[i].set_xlim(xlim)
+            else:
+                axes[i].set_xlim(0.15, 0.78)
+                
+            if ylim != (None, None):
+                axes[i].set_ylim(ylim)
+            else:
+                axes[i].set_ylim(-0.88,0.1)
+            
+            # Take out Y markings from plots after first one
+            if i != 0:
+                axes[i].set_ylabel("")
+                axes[i].set_xlabel("R [m]")
+                axes[i].tick_params(axis="y", which="both", left=False,labelleft=False)
+                
+        # Add colorbar
+        axes[-1] = fig.add_subplot(gs0a[0,-1])   
+        norm = xbout.plotting.utils._create_norm(logscale, None, vmin, vmax)
+        sm = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
+        cbar = plt.colorbar(mappable = sm, cax = axes[-1], label = param)
+
+        
+        slider = RangeSlider(
+            fig.add_axes([0.2, 0.05, 0.5, 0.1]), "Colour limits",   # left, bottom, width, height
+            self.min, self.max,
+            orientation = "horizontal",
+            valinit = (self.min, self.max)
+            )
+
+        artists = []
+        
+        # This is to account for missing artists if one case has no param
+        for i in range(num_cases):
+            try:
+                artists.append(axes[i].collections[0])
+            except:
+                pass
+        # artists = artists.append(cbar)
+
+        def update(val):
+            slider.ax.set_ylim(self.min, self.max) # This is inexplicably needed otherwise it freezes
+            
+            cbar.norm.vmin = val[0]
+            cbar.norm.vmax = val[1]
+                
+            for i, artist in enumerate(artists):
+                if cases[list(cases.keys())[i]]["code"] == "solps" and logscale == True:
+                    artist.norm.vmin = np.log10(val[0])
+                    artist.norm.vmax = np.log10(val[1])
+                else:
+                    artist.norm.vmin = val[0]
+                    artist.norm.vmax = val[1]
+                
+                fig.canvas.draw_idle()
+                fig.canvas.flush_events() # https://stackoverflow.com/questions/64789437/what-is-the-difference-between-figure-show-figure-canvas-draw-and-figure-canva
+                
+        slider.on_changed(update)
+
+            
+        
+    def find_ranges(self):
+        """
+        Find min and max for plotting.
+        Only uses hermes so far
+        """
+        max = []
+        min = []
+        
+        for casename in self.cases.keys():
+            case = self.cases[casename]
+            if case["code"] == "hermes":
+                max.append(case["ds"][self.param].values.max())
+                min.append(case["ds"][self.param].values.min())
+                
+        return np.max(max), np.min(min)
+    
+    
+class viewer_2d_next():
+    """
+    Pass a case:
+    case = {
+        <case_name> : {"name" : <case name>, "code" : "hermes", "ds" : <hermes-3 dataset>},
+        <case_name> : {"name" : <case name>, "code" : "solps", "path" : <path to SOLPS dir>}
+        <case_name> : {"code" : "soledge", "path": <path to SOLEDGE dir>}
+        }
+    """
+    def __init__(self,
+                 param,
+                 cases,
+                 vmin = None,
+                 vmax = None,
+                 logscale = True,
+                 xlim = (None,None),
+                 ylim = (None,None),
                  dpi = 120,
                  cmap = "Spectral_r"):
         
@@ -335,11 +525,30 @@ class viewer_2d():
                 axes[i].set_title(f"Hermes-3\{casename}")
                 
             elif model["code"] == "solps":
-                plot_2d(fig, axes[i], where = model["path"], what = name_parser(param,"solps"), cmap = "Spectral_r", scale = ("log" if logscale is True else "linear"), vmin = vmin, vmax = vmax, plot_cbar = False)
-                axes[i].set_title(f"SOLPS\{casename}")
+                try:
+                    solps_name = name_parser(param,"solps")
+                    
+                except:
+                    print(f"Parameter {param} not found in SOLPS")
+                    solps_name = None
+                    
+                if solps_name != None:
+                    plot_2d(fig, axes[i], where = model["path"], what = name_parser(param,"solps"), cmap = "Spectral_r", scale = ("log" if logscale is True else "linear"), vmin = vmin, vmax = vmax, plot_cbar = False)
+                    axes[i].set_title(f"SOLPS\{casename}")
                 
-            axes[i].set_ylim(-0.88,0.1)
-            axes[i].set_xlim(0.15, 0.78)
+                else:
+                    axes[i].set_title(f"SOLPS\{casename}: {param} not found")
+                
+            if xlim != (None, None):
+                axes[i].set_xlim(xlim)
+            else:
+                axes[i].set_xlim(0.15, 0.78)
+                
+            if ylim != (None, None):
+                axes[i].set_ylim(ylim)
+            else:
+                axes[i].set_ylim(-0.88,0.1)
+
             
             # Take out Y markings from plots after first one
             if i != 0:
@@ -431,7 +640,8 @@ def name_parser(x, code):
         "Ne" : "ne",
         "Te" : "te",
         "Td+" : "ti",   
-        "Nd" : "pdenn"   # Custom made by matteo. Atoms: pdena, Molecules: pdenm         
+        "Nd" : "pdenn",   # Combined atoms+molecules. Custom made by matteo. Atoms: pdena, Molecules: pdenm
+        "Td" : "tdena"    # Compare only atom temperature, ignore molecules (more physical)
     }
     
     if code == "solps":
@@ -458,3 +668,116 @@ def file_read(filename):
         data = pkl.load(filename)
         
     return data
+
+
+def lineplot(
+    cases,
+    mode = "log",
+    colors = ["black", "red", "black", "red", "navy", "limegreen", "firebrick",  "limegreen", "magenta","cyan", "navy"],
+    params = ["Td+", "Te", "Td", "Ne", "Nd"],
+    regions = ["imp", "omp", "outer_lower"],
+    dpi = 120
+    ):
+    
+    marker = "o"
+    ms = 0
+    lw = 1.5
+    set_ylims = dict()
+    set_yscales = dict()
+
+
+    if mode == "sol":
+        
+        set_ylims = {
+        "imp"         : {"Td+": (0, 300), "Te": (0, 250), "Ne": (1e15, 3e19), "Nd": (0.15e10, 1e18)},
+        "omp"         : {"Td+": (0, 250), "Te": (0, 250), "Ne": (1e17, 0.5e20), "Nd": (1e10, 1e16)},
+        "outer_lower" : {"Td+": (0, 250), "Te": (0, 250), "Ne": (None, 4e18), "Nd": (0, None)},
+        }
+        
+        set_yscales = {
+        "imp" : {"Td+": "linear", "Te": "linear", "Ne": "linear", "Nd": "linear"},
+        "omp" : {"Td+": "linear", "Te": "linear", "Ne": "linear", "Nd": "linear"},
+        "outer_lower" : {"Td+": "linear", "Te": "linear", "Ne": "linear", "Nd": "log"},
+        }
+        
+        xlims = (-0.01, None)
+        
+    if mode == "core":
+        
+        set_ylims = {
+        "imp"         : {"Td+": (0, 2600), "Te": (0, 2500), "Ne": (1e15, 8e19), "Nd": (0.15e10, 1e16)},
+        "omp"         : {"Td+": (0, 2600), "Te": (0, 2500), "Ne": (1e17, 1e20), "Nd": (1e10, 1e16)},
+        "outer_lower" : {"Td+": (0, 250), "Te": (0, 250), "Ne": (None, 4e18), "Nd": (0, None)},
+        }
+        
+        set_yscales = {
+        "imp" : {"Td+": "linear", "Te": "linear", "Ne": "linear", "Nd": "linear"},
+        "omp" : {"Td+": "linear", "Te": "linear", "Ne": "linear", "Nd": "linear"},
+        "outer_lower" : {"Td+": "linear", "Te": "linear", "Ne": "linear", "Nd": "log"},
+        }
+        
+        xlims = (None, 0.01)
+        # xlims = (None, None)
+        
+        
+    elif mode == "log":
+        
+        set_ylims = {
+        "imp"         : {"Td+": (0, 2600), "Te": (0, 2600), "Ne": (1e15, 2e20), "Nd": (1e13, 0.15e18)},
+        "omp"         : {"Td+": (0, 2600), "Te": (0, 2600), "Ne": (1e17, 2e20), "Nd": (1e14, 1e18)},
+        "outer_lower" : {"Td+": (0, 250), "Te": (0, 250), "Ne": (None, 4e18), "Nd": (0, None)},
+        }
+            
+        set_yscales = {
+        "omp" : {"Td+": "log", "Te": "log", "Ne": "log", "Nd": "log"},
+        "imp" : {"Td+": "log", "Te": "log", "Ne": "log", "Nd": "log"},
+        "outer_lower" : {"Td+": "linear", "Te": "linear", "Ne": "linear", "Nd": "log"},
+        }
+        
+        xlims = (None, None)
+
+
+
+    for region in regions:
+
+        fig, axes = plt.subplots(1,len(params), dpi = 150, figsize = (4*len(params),3.5))
+        fig.subplots_adjust(hspace = 0.1, wspace = 0.25)
+
+        
+        for i, param in enumerate(params):
+            for j, name in enumerate(cases.keys()):
+                if region in cases[name].regions.keys():
+                    data = cases[name].regions[region]
+                    if param in data.columns:
+                        if "SOLPS" in name:
+                            ls = "--"
+                        else:
+                            ls = "-"
+                        axes[i].plot(data.index, data[param], label = name, c = colors[j], marker = marker, ms = ms, lw = lw, ls = ls)
+                    
+            if param in set_yscales[region].keys():
+                axes[i].set_yscale(set_yscales[region][param])
+                
+            if param in set_ylims[region].keys():
+                if set_ylims[region][param] != (None, None):
+                    axes[i].set_ylim(set_ylims[region][param])
+
+        
+            # axes[i].set_xlim(cases["Hermes-3"].regions[region].index[0] - 0.01, cases["Hermes-3"].regions[region].index[-1] * 1.3)
+            if xlims != (None, None):
+                axes[i].set_xlim(xlims)
+            
+            axes[i].grid(which="both", alpha = 0.2)
+            axes[i].set_xlabel("Distance from separatrix [m]")
+            # axes[i].legend()
+            axes[i].set_title(f"{region}: {param}")
+            
+        legend_items = []
+        for j, name in enumerate(cases.keys()):
+            if "SOLPS" in name:
+                ls = "--"
+            else:
+                ls = "-"
+            legend_items.append(mpl.lines.Line2D([0], [0], color=colors[j], lw=2, ls = ls))
+            
+        fig.legend(legend_items, cases.keys(), loc = "upper left", bbox_to_anchor=(0.9,0.9))
