70a40bc5a6951231916747e0c3a758697502e297
Mike Kryjak <mikekryjak@gmail.com>
2023-08-20 15:01:17 +0100

    Add more to cpp tools

--- hermes3/hermes-cpp-tools.py
+++ hermes3/hermes-cpp-tools.py
@@ -23,3 +23,38 @@ if (sol_recycling) {
     }
 
   }
+  
+  output << std::string("\n******************************************\n");
+  output << s1->first << s2->first << std::string(": ") << collision_rates[s1->first][s2->first];
+  output << std::string("\n******************************************");
+
+
+for(int ix=0; ix < mesh->LocalNx ; ix++){
+      for(int iy=0; iy < mesh->LocalNy ; iy++){
+          for(int iz=0; iz < mesh->LocalNz; iz++){
+
+            BoutReal gx = mesh->getGlobalXIndex(ix);
+            BoutReal gy = mesh->getGlobalYIndex(iy);
+            BoutReal gz = mesh->getGlobalZIndex(iz);
+
+            if (gx == 3) {
+
+              output << nu_12(ix,iy,iz) << "\n";
+            }
+            
+            // output <<"MYPE: " << mype << ", (" << gx << ", " << gy << ", " << gz << "), nu_12 = " << nu_12(ix,iy,iz);
+            // if ((gy > 39.5) and (gy <40.5)) {
+
+            //   std::string string_count = std::string("(") + std::to_string(gx) + std::string(")");
+            //   output << string_count + std::string(": ") + std::to_string(nu_12(ix,iy,iz)) + std::string("; ");
+
+            // }
+            // output << "("" << ix << "Y:" << iy << "Z:" << iz << "T:" << Tn(ix, iy, iz) << "  ";
+            
+          }
+      }
+          
+          # SETS WIDTH (precision?)
+if (gx == 3) {
+              output << std::setw(10) << nu_12(ix,iy,iz) << "\t" << nu(ix, iy, iz) << "\n";
+            }
