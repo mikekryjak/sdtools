89667438c59ecfda980bcf4b5f8725d10b6ea6c2
Mike Kryjak <mikekryjak@gmail.com>
2023-09-09 19:10:24 +0100

    Fixed circular import of squash/db/load

--- hermes3/load.py
+++ hermes3/load.py
@@ -8,6 +8,7 @@ import matplotlib as mpl
 import os, sys
 import traceback
 import platform
+from datetime import datetime as dt
 from scipy import stats
 from boututils.datafile import DataFile
 from boutdata.collect import collect
@@ -18,7 +19,6 @@ from hermes3.utils import *
 from hermes3.named_selections import *
 from hermes3.plotting import *
 from hermes3.fluxes import *
-from hermes3.case_db import *
 
 
 class Load:
@@ -1331,7 +1331,64 @@ class Case:
 
 
 
+def squash(casepath, verbose = True, force = False):
+    """
+    Checks if squashed file exists. If it doesn't, or if it's older than the dmp files, 
+    then it creates a new squash file.
+    
+    Inputs
+    ------
+    Casepath is the path to the case directory
+    verbose gives you extra info prints
+    force always recreates squash file
+    """
+    
+    datapath = os.path.join(casepath, "BOUT.dmp.*.nc")
+    inputfilepath = os.path.join(casepath, "BOUT.inp")
+    squashfilepath = os.path.join(casepath, "BOUT.squash.nc") # Squashoutput hardcoded to this filename
 
+    recreate = True if force is True else False   # Override to recreate squashoutput
+    squash_exists = False
+    
+    if verbose is True: print(f"- Looking for squash file")
+        
+    if "BOUT.squash.nc" in os.listdir(casepath):  # Squash file found?
+        
+        squash_exists = True
+        
+        squash_date = os.path.getmtime(squashfilepath)
+        dmp_date = os.path.getmtime(os.path.join(casepath, "BOUT.dmp.0.nc"))
+        
+        squash_date_string = dt.strftime(dt.fromtimestamp(squash_date), r"%m/%d/%Y, %H:%M:%S")
+        dmp_date_string = dt.strftime(dt.fromtimestamp(dmp_date), r"%m/%d/%Y, %H:%M:%S")
+        
+        if verbose is True: print(f"- Squash file found. squash date {squash_date_string}, dmp file date {dmp_date_string}") 
+        
+        if dmp_date > squash_date:   #Recreate if squashoutput is too old
+            recreate = True
+            print(f"- dmp files are newer than the squash file! Recreating...") 
+            
+    else:
+        if verbose is True: print(f"- Squashoutput file not found, creating...")
+        recreate = True
+        
+
+    if recreate is True:
+        
+        if squash_exists is True:  # Squashoutput will not overwrite, so we delete the file first
+            os.remove(squashfilepath)
+            
+        squashoutput(
+            datadir = casepath,
+            outputname = squashfilepath,
+            xguards = True,   # Take all xguards
+            yguards = "include_upper",  # Take all yguards (yes, confusing name)
+            parallel = False,   # Seems broken atm
+            quiet = verbose
+        )
+        
+        if verbose is True: print(f"- Done")
+            
 
 
 
