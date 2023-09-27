1a3f469cf072f34396acc9c23e825463fc64c14c
Mike Kryjak <mikekryjak@gmail.com>
2023-09-27 10:40:41 +0100

    Replace mpl stylesheet with rcparams

--- general/plotstyle.py
+++ general/plotstyle.py
@@ -1,2 +1,26 @@
 import matplotlib.pyplot as plt
-plt.style.use(r"C:\Users\mikek\OneDrive\Project\python-packages\sdtools\general\mike.mplstyle")
+import matplotlib as mpl
+# plt.style.use(r"C:\Users\mikek\OneDrive\Project\python-packages\sdtools\general\mike.mplstyle")
+
+
+mpl.rcParams["mathtext.fontset"] = "stix"
+mpl.rcParams["mathtext.default"] = "regular"
+mpl.rcParams["axes.grid"] = True
+mpl.rcParams["axes.grid.which"] = "both"
+mpl.rcParams["axes.formatter.limits"] = (-10,10)
+mpl.rcParams["axes.formatter.useoffset"] = False
+mpl.rcParams["axes.autolimit_mode"] = "data"
+mpl.rcParams["axes.xmargin"] = 0.1
+mpl.rcParams["axes.ymargin"] = 0.1
+mpl.rcParams["xtick.major.width"] = 1
+mpl.rcParams["ytick.major.width"] = 1
+mpl.rcParams["grid.linestyle"] = "-"
+mpl.rcParams["grid.linewidth"] = 0.3
+mpl.rcParams["grid.alpha"] = 0.15
+mpl.rcParams["legend.loc"] = "best"
+mpl.rcParams["legend.fontsize"] = "medium"
+mpl.rcParams["legend.markerscale"] = 2.0
+mpl.rcParams["legend.framealpha"] = 0.3
+mpl.rcParams["figure.figsize"] = (6,5)
+mpl.rcParams["figure.dpi"] = 120
+mpl.rcParams["figure.facecolor"] = "white"
