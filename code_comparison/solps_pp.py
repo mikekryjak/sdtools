5bbd0ade0d6bf9fbbc58c9c91eb50245df07e274
Mike Kryjak <mikekryjak@gmail.com>
2023-10-12 19:12:30 +0100

    Add a lot to solps_pp

--- code_comparison/solps_pp.py
+++ code_comparison/solps_pp.py
@@ -82,6 +82,119 @@ class SOLPScase():
         
         # bal.close()
         
+    def close(self):
+        self.bal.close()
+        
+    def plot_2d(self, param,
+             ax = None,
+             fig = None,
+             norm = None,
+             data = np.array([]),
+             cmap = "Spectral_r",
+             antialias = False,
+             linecolor = "k",
+             linewidth = 0,
+             vmin = None,
+             vmax = None,
+             logscale = False,
+             alpha = 1,
+             separatrix = True):
+        
+        if len(data)==0:
+            data = self.bal[param][:]
+        
+        if vmin == None:
+            vmin = data.min()
+        if vmax == None:
+            vmax = data.max()
+        if norm == None:
+            norm = create_norm(logscale, norm, vmin, vmax)
+        if ax == None:
+            fig, ax = plt.subplots(dpi = 150)
+        
+
+        # Following SOLPS convention: X poloidal, Y radial
+        crx = self.bal["crx"]
+        cry = self.bal["cry"]
+        
+        Nx = crx.shape[2]
+        Ny = crx.shape[1]
+        
+        print(data.shape)
+        print(Nx, Ny)
+        # print(cry.shape)
+        
+        
+        # In hermes-3 and needed for plot: lower left, lower right, upper right, upper left, lower left
+        # SOLPS crx structure: lower left, lower right, upper left, upper right
+        # So translating crx is gonna be 0, 1, 3, 2, 0
+        # crx is [corner, Y(radial), X(poloidal)]
+        idx = [np.array([0, 1, 3, 2, 0])]
+
+        # Make polygons
+        patches = []
+        for i in range(Nx):
+            for j in range(Ny):
+                p = mpl.patches.Polygon(
+                    np.concatenate([crx[:,j,i][tuple(idx)], cry[:,j,i][tuple(idx)]]).reshape(2,5).T,
+                    
+                    fill=False,
+                    closed=True,
+                )
+                patches.append(p)
+                
+        # Polygon colors
+        colors = data.transpose().flatten()
+        polys = mpl.collections.PatchCollection(
+            patches, alpha = alpha, norm = norm, cmap = cmap, 
+            antialiaseds = antialias,
+            edgecolors = linecolor,
+            linewidths = linewidth,
+            joinstyle = "bevel")
+
+        polys.set_array(colors)
+        
+        if fig != None:
+            fig.colorbar(polys)
+        ax.add_collection(polys)
+        ax.set_aspect("equal")
+        
+        if separatrix is True:
+            self.plot_separatrix(ax = ax)
+            
+    def plot_separatrix(self, ax):
+
+        # Author: Matteo Moscheni
+        # E-mail: matteo.moscheni@tokamakenergy.co.uk
+        # February 2022
+
+        # try:    b2fgmtry = load_pickle(where = where, verbose = False, what = "b2fgmtry")
+        # except: b2fgmtry = read_b2fgmtry(where = where, verbose = False, save = True)
+        b2fgmtry = self.g
+        colour = "white"
+
+        iy = int(b2fgmtry['ny'] / 2)
+
+        if len(b2fgmtry['rightcut']) == 2:
+            ix_mid = int((b2fgmtry['rightcut'][1] + b2fgmtry['leftcut'][1]) / 2 - 1)
+
+            for ix in range(ix_mid - 2):
+                x01 = [b2fgmtry['crx'][ix,iy,0], b2fgmtry['crx'][ix,iy,1]]
+                y01 = [b2fgmtry['cry'][ix,iy,0], b2fgmtry['cry'][ix,iy,1]]
+                ax.plot(x01, y01, c = colour, lw = 2)
+
+            for ix in range(ix_mid, b2fgmtry['nx']):
+                x01 = [b2fgmtry['crx'][ix,iy,0], b2fgmtry['crx'][ix,iy,1]]
+                y01 = [b2fgmtry['cry'][ix,iy,0], b2fgmtry['cry'][ix,iy,1]]
+                ax.plot(x01, y01, c = colour, lw = 2)
+        else:
+            for ix in range(b2fgmtry['nx']):
+                x01 = [b2fgmtry['crx'][ix,iy,0], b2fgmtry['crx'][ix,iy,1]]
+                y01 = [b2fgmtry['cry'][ix,iy,0], b2fgmtry['cry'][ix,iy,1]]
+                ax.plot(x01, y01, c = colour, lw = 2)
+
+        return
+        
     def get_1d_radial_data(
         self,
         param,
@@ -108,4 +221,26 @@ class SOLPScase():
         return df
         
         
-        
+def create_norm(logscale, norm, vmin, vmax):
+    if logscale:
+        if norm is not None:
+            raise ValueError(
+                "norm and logscale cannot both be passed at the same time."
+            )
+        if vmin * vmax > 0:
+            # vmin and vmax have the same sign, so can use standard log-scale
+            norm = mpl.colors.LogNorm(vmin=vmin, vmax=vmax)
+        else:
+            # vmin and vmax have opposite signs, so use symmetrical logarithmic scale
+            if not isinstance(logscale, bool):
+                linear_scale = logscale
+            else:
+                linear_scale = 1.0e-5
+            linear_threshold = min(abs(vmin), abs(vmax)) * linear_scale
+            if linear_threshold == 0:
+                linear_threshold = 1e-4 * vmax   # prevents crash on "Linthresh must be positive"
+            norm = mpl.colors.SymLogNorm(linear_threshold, vmin=vmin, vmax=vmax)
+    elif norm is None:
+        norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+
+    return norm
