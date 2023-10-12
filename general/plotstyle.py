a67264216eef4b5f4e7213d9a7e795a00ff16d48
Mike Kryjak <mikekryjak@gmail.com>
2023-10-12 19:12:17 +0100

    Improve color themes

--- general/plotstyle.py
+++ general/plotstyle.py
@@ -11,8 +11,9 @@ mpl.rcParams["axes.grid"] = True
 mpl.rcParams["axes.grid.which"] = "both"
 mpl.rcParams["axes.formatter.limits"] = (-3,3)
 mpl.rcParams["axes.formatter.useoffset"] = False
-# mpl.rcParams["axes.prop_cycle"] = cycler("color", ["teal", "darkorange", "firebrick", "limegreen", "mediumblue", "darkorchid", "deeppink"])
-mpl.rcParams["axes.prop_cycle"] = cycler("color", ["teal", "darkorange", "#fb8072", "#b3de69", "#80b1d3", "darkorchid", "deeppink"])
+mpl.rcParams["axes.prop_cycle"] = cycler("color", ["teal", "darkorange", "firebrick", "mediumblue", "limegreen", "darkorchid", "deeppink", 
+                                                   "#1E90FF", "forestgreen", "#4169e1", "darkgoldenrod", "#9370db", "crimson", "#2f4f4f" ])
+# mpl.rcParams["axes.prop_cycle"] = cycler("color", ["teal", "darkorange", "#fb8072", "#4daf4a", "#1f78b4", "darkorchid", "deeppink"])
 mpl.rcParams["axes.autolimit_mode"] = "data"
 mpl.rcParams["axes.xmargin"] = 0.1
 mpl.rcParams["axes.ymargin"] = 0.1
@@ -32,3 +33,23 @@ mpl.rcParams["legend.framealpha"] = 0.3
 mpl.rcParams["figure.figsize"] = (6,5)
 mpl.rcParams["figure.dpi"] = 120
 mpl.rcParams["figure.facecolor"] = "white"
+
+def change_colors(theme):
+    if theme == "default":
+        mpl.rcParams["axes.prop_cycle"] = cycler("color", ["teal", "darkorange", "firebrick", "limegreen", "mediumblue", "darkorchid", "deeppink"])
+    elif theme == "pastel":
+        mpl.rcParams["axes.prop_cycle"] = cycler("color", ["teal", "darkorange", "#fb8072", "#4daf4a", "#1f78b4", "darkorchid", "deeppink"])
+        
+        
+"""
+Here are seven colors that should work well with your existing colors:
+
+Dodger Blue
+Forest Green
+Royal Blue
+Dark Goldenrod
+Medium Purple
+Crimson
+Dark Slate Gray
+
+"""
