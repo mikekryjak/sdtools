ac723e8b3a27b0b829c025736ad934201ea1fbbd
Mike Kryjak <mikekryjak@gmail.com>
2025-01-27 12:30:44 +0000

    Improved color cycler control

--- general/plotstyle.py
+++ general/plotstyle.py
@@ -14,14 +14,30 @@ mpl.rcParams["axes.formatter.useoffset"] = False
 # mpl.rcParams["axes.prop_cycle"] = cycler("color", ["teal", "darkorange", "firebrick", "limegreen", "darkorchid", "deeppink", 
 #                                                    "#1E90FF", "forestgreen", "#4169e1", "darkgoldenrod", "#9370db", "crimson", "#2f4f4f" ])
 
-newcycle = ["#0C9481", "#EC9714", "#18569A", "#EC6D14"]
-newcycle = ["#02A992", "#105BAF", "#FF9C04", "#FF6C04"]
-newcycle = ["#02A992", "#0B76AA", "#FF8A04", "#FF6C04"]  # Hue 206, dist 15deg
-newcycle = ["#43B7C2", "#024B79", "#FFAD48", "#BA5800"]
-newcycle = ["#43B7C2", "#FFAD48", "#024B79", "#BA5800"]
-mpl.rcParams["axes.prop_cycle"] = cycler("color", newcycle + ["darkorchid", "limegreen", "deeppink", 
-                                                   "#1E90FF", "forestgreen", "#4169e1", "darkgoldenrod", "#9370db", "crimson", "#2f4f4f" ])
+# newcycle = ["#0C9481", "#EC9714", "#18569A", "#EC6D14"]
+# newcycle = ["#02A992", "#105BAF", "#FF9C04", "#FF6C04"]
+# newcycle = ["#02A992", "#0B76AA", "#FF8A04", "#FF6C04"]  # Hue 206, dist 15deg
+# newcycle = ["#43B7C2", "#024B79", "#FFAD48", "#BA5800"]
+# newcycle = ["#43B7C2", "#FFAD48", "#024B79", "#BA5800"]
+
+# mpl.rcParams["axes.prop_cycle"] = cycler("color", newcycle + ["darkorchid", "limegreen", "deeppink", 
+#                                                    "#1E90FF", "forestgreen", "#4169e1", "darkgoldenrod", "#9370db", "crimson", "#2f4f4f" ] + default_cycle)
 # mpl.rcParams["axes.prop_cycle"] = cycler("color", ["teal", "darkorange", "#fb8072", "#4daf4a", "#1f78b4", "darkorchid", "deeppink"])
+
+default_cycler = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+def change_colors(theme):
+    
+    if theme == "default":
+        mpl.rcParams["axes.prop_cycle"] = cycler("color", default_cycler)
+    elif theme == "standard":
+        mpl.rcParams["axes.prop_cycle"] = cycler("color", ["teal", "darkorange", "firebrick", "limegreen", "mediumblue", "darkorchid", "deeppink", "#1E90FF", "forestgreen", "#4169e1", "darkgoldenrod", "#9370db", "crimson", "#2f4f4f"] + default_cycler)
+    elif theme == "pastel":
+        mpl.rcParams["axes.prop_cycle"] = cycler("color", ["teal", "darkorange", "#fb8072", "#4daf4a", "#1f78b4", "darkorchid", "deeppink"] + default_cycler)
+
+change_colors("standard")
+
+
 mpl.rcParams["axes.autolimit_mode"] = "data"
 
 mpl.rcParams["axes.xmargin"] = 0.1   # Margins for auto lims
@@ -70,11 +86,7 @@ mpl.rcParams['savefig.transparent'] = False
 mpl.rcParams['font.sans-serif'] = ['DejaVu Sans', 'Helvetica', 'Tahoma', 'DejaVu Sans',
                                'Lucida Grande', 'Verdana']
 
-def change_colors(theme):
-    if theme == "default":
-        mpl.rcParams["axes.prop_cycle"] = cycler("color", ["teal", "darkorange", "firebrick", "limegreen", "mediumblue", "darkorchid", "deeppink"])
-    elif theme == "pastel":
-        mpl.rcParams["axes.prop_cycle"] = cycler("color", ["teal", "darkorange", "#fb8072", "#4daf4a", "#1f78b4", "darkorchid", "deeppink"])
+
         
         
 """
