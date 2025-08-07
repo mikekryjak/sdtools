1a3c610522c3a153b3477c7abd06b1494620d469
Mike Kryjak <mikekryjak@gmail.com>
2025-08-07 12:00:29 +0100

    Improvements to Balance1D and new flux balance plotter

--- hermes3/balance1d.py
+++ hermes3/balance1d.py
@@ -15,6 +15,7 @@ class Balance1D():
         verbose = True, 
         use_sheath_diagnostic = False,
         override_vi = False,
+        sheath_flux_method = "flux_diagnostic",
         impurity_name = "Ar",
         ):
         """
@@ -49,13 +50,13 @@ class Balance1D():
         self.override_vi = override_vi
         self.verbose = verbose
         self.get_properties()  # Get settings etc
-        self.reconstruct_sheath_fluxes()  # Now have .sheath dict with properties
+        self.calculate_sheath_fluxes(method = sheath_flux_method)  # Now have .sheath dict with properties
         
-        # Check for sheath diagnostic variables
-        self.sheath_diagnostics = True
-        sheath_flux_candidates = [var for var in ds.data_vars if re.search("S.*\+_sheath", var)]
-        if len(sheath_flux_candidates) == 0:
-            self.sheath_diagnostics = False
+        # # Check for sheath diagnostic variables
+        # self.sheath_diagnostics = True
+        # sheath_flux_candidates = [var for var in ds.data_vars if re.search("S.*\\+_sheath", var)]
+        # if len(sheath_flux_candidates) == 0:
+        #     self.sheath_diagnostics = False
         
         # Check if more than one time slice
         if "t" in ds.dims:
@@ -85,7 +86,7 @@ class Balance1D():
         ### Particle balance
         pbal = {}
         pbalsum = {}
-        for param in ["Sd+_src", "Sd_src", "Sd+_feedback", "Sd+_iz", "Sd+_rec", "Sd_target_recycle", "Sd+_sheath"]:
+        for param in ["Sd+_src", "Sd_src", "Sd+_feedback", "Sd+_iz", "Sd+_rec", "Sd_target_recycle"]:
             if param in ds:
                 pbal[param] = integral(dom[param])
             else:
@@ -97,7 +98,7 @@ class Balance1D():
         ### Heat balance
         hbal = {}
         hbalsum = {}
-        for param in ["Pd+_src", "Pe_src", "Pd_src", "Rd+_ex", "Rd+_rec", f"R{self.impurity_name}", "Ed_target_recycle", "Ee_sheath", "Ed+_sheath"]:
+        for param in ["Pd+_src", "Pe_src", "Pd_src", "Rd+_ex", "Rd+_rec", f"R{self.impurity_name}", "Ed_target_recycle"]:
             if param in ds:
                 hbal[param] = integral(dom[param])*1e-6   # MW
 
@@ -113,17 +114,12 @@ class Balance1D():
                     print(f"Warning! {param} not found, results may be incorrect")
                 hbal[param] = np.zeros_like(integral(dom["Ne"]))
                 
-        if self.use_sheath_diagnostic is False or self.sheath_diagnostics is False:
-            if self.verbose: print("|||WARNING: Overwriting sheath diagnostics with calculated values")
-            hbal["Ee_sheath"] = self.sheath["hfe"]
-            hbal["Ed+_sheath"] = self.sheath["hfi"]
-            pbal["Sd+_sheath"] = self.sheath["pfi"]
+        hbal["Ee_sheath"] = self.sheath["hfe"]
+        hbal["Ed+_sheath"] = self.sheath["hfi"]
+        pbal["Sd+_sheath"] = self.sheath["pfi"]
         self.pbal = pbal
         self.hbal = hbal
-        
-        if self.sheath_diagnostics is False:
-            if self.verbose: print("Sheath diagnostics not available, attempting to reconstruct...")
-            self.reconstruct_sheath_fluxes()
+    
             
         self.terms_extracted = True
         
@@ -163,7 +159,7 @@ class Balance1D():
         hbalsum["power_out"] = hbalsum["R_sum"] + hbalsum["sheath_sum"]
         
         hbalerror = {}
-        hbalerror["error"] = (hbalsum["R_and_sources_sum"] + hbalsum["sheath_sum"]) / hbalsum["sources_sum"]
+        hbalerror["error"] = (hbalsum["R_and_sources_sum"] - hbalsum["sheath_sum"]) / hbalsum["sources_sum"]
         
         self.pbalsum = pbalsum
         self.hbalsum = hbalsum
@@ -224,7 +220,17 @@ class Balance1D():
             self.phi_wall = 0
             
             
-    def reconstruct_sheath_fluxes(self, override_vi = False):
+    def get_sheath_fluxes(self):
+        """
+        Calculate sheath fluxes from flux diagnostic variables
+        """
+        
+        
+    def calculate_sheath_fluxes(self,
+                                method = "flux_diagnostic",
+                                override_vi = False,
+                                calculate_ve = False
+                                  ):
         """
         Calculate sheath fluxes from the fields without need for diagnostic variable
         
@@ -258,18 +264,37 @@ class Balance1D():
 
         cssheath = np.sqrt((sheath_ion_polytropic * tisheath*qe + Zi * tesheath*qe) / Mi)   # [m/s] Bohm criterion sound speed
 
-        ion_sum = Zi * nisheath * cssheath   # Sheath current
-        phisheath = tesheath * np.log(np.sqrt(tesheath / (Me * 2*np.pi)) * (1 - Ge) * nesheath / ion_sum)  # [V] sheath potential, (note Neumann BC)
-        vesheath = np.sqrt(tesheath / (2*np.pi * Me)) * (1 - Ge) * np.exp(-(phisheath - phi_wall)/tesheath)
+        if calculate_ve:
+            ion_sum = Zi * nisheath * cssheath   # Sheath current
+            phisheath = tesheath * np.log(np.sqrt(tesheath / (Me * 2*np.pi)) * (1 - Ge) * nesheath / ion_sum)  # [V] sheath potential, (note Neumann BC)
+            vesheath = np.sqrt(tesheath / (2*np.pi * Me)) * (1 - Ge) * np.exp(-(phisheath - phi_wall)/tesheath)
+        else:
+            ion_sum = 0
+            phisheath = 0
+            
         
         if self.override_vi == True:
             visheath = cssheath
 
-        pfi_sheath = -1 * visheath * nisheath * dasheath   # [s^-1] ion particle flux into domain 
-        pfe_sheath = -1 * vesheath * nesheath * dasheath   # [s^-1] electron particle flux into domain 
+        if method == "reconstruction":
+            pfi_sheath = -1 * visheath * nisheath * dasheath   # [s^-1] ion particle flux into domain 
+            pfe_sheath = -1 * vesheath * nesheath * dasheath   # [s^-1] electron particle flux into domain 
 
-        hfi_sheath = pfi_sheath * self.gamma_i * tisheath*qe * 1e-6   # [MW] electron heat flux into domain
-        hfe_sheath = pfe_sheath * self.gamma_e * tesheath*qe * 1e-6   # [MW] electron heat flux into domain
+            hfi_sheath = pfi_sheath * self.gamma_i * tisheath*qe * 1e-6   # [MW] electron heat flux into domain
+            hfe_sheath = pfe_sheath * self.gamma_e * tesheath*qe * 1e-6   # [MW] electron heat flux into domain
+            
+        elif method == "flux_diagnostic":
+            pfi_sheath = ds["pfd+_tot_ylow"].isel(pos=-2)
+            pfe_sheath = pfi_sheath
+            hfe_sheath = ds["efe_tot_ylow"].isel(pos=-2) * 1e-6
+            hfi_sheath = ds["efd+_tot_ylow"].isel(pos=-2) * 1e-6
+            
+        elif method == "source_diagnostic":
+            pfi_sheath = (ds["Sd+_sheath"] * ds["dv"]).values.sum()
+            pfe_sheath = pfi_sheath
+            hfi_sheath = (ds["Ed+_sheath"] * ds["dv"]).values.sum() * 1e-6
+            hfe_sheath = (ds["Ee_sheath"] * ds["dv"]).values.sum() * 1e-6
+            
         
         self.sheath = dict(
             da = dasheath,
@@ -349,7 +374,9 @@ class Balance1D():
         ax.set_ylabel("[MW]")
         ax.set_xlabel("Time [s]")
         
-    def plot_flux_balance(self, 
+    
+        
+    def plot_flux_balance_old(self, 
                           use_diagnostics = True,
                           xlims = (None, None),
                           flux_style = {}):
@@ -415,7 +442,7 @@ class Balance1D():
             
         # ax.set_yscale("log")
         
-    def plot_flux_balance2(self, xlims = (None, None), title = ""):
+    def plot_flux_balance2_old(self, xlims = (None, None), title = ""):
         """
         This comes from Ben
         """
@@ -702,10 +729,88 @@ class Balance1D():
         print("Diagnostic output")
         print("----------------------")
         error = hbalerror["error"][-1] if self.time else hbalerror["error"]
-        print(f"Error: (R + Src + Sheath) / Src: {error:.2%}")
+        print(f"Error: (R + Src - Sheath) / Src: {error:.2%}")
         
     def check_bug(self):
         if type(self.pbal["Sd+_src"]) == np.float64:
             raise Exception("BUG FOUND")
         else:
             print("No bug found")
+            
+            
+class FluxBalance():
+    
+    def __init__(self, ds, include_neutrals = True):
+        self.ds = ds
+        self.include_neutrals = include_neutrals
+        if "t" in ds.coords:
+            ds = self.ds = ds.isel(t=-1)
+            
+        dom = self.dom = ds.isel(pos = slice(2,-1))  # Remove guard cells
+        domp = self.domp = dom.shift(pos = -1)  # # Shift to get the next point in the domain
+        self.xaxis = dom["pos"].values.max() - dom["pos"].values + 1e-3
+    
+    def plot_total_balance(self):
+        
+        fig, ax = plt.subplots(figsize = (10, 4))
+
+        dom = self.dom
+        domp = self.domp
+        x = self.xaxis
+
+
+        ## Divergences
+        # flow_LHS - flow_RHS
+        electrons = dom[f"efe_tot_ylow"] - domp[f"efe_tot_ylow"]
+        ions = dom[f"efd+_tot_ylow"] - domp[f"efd+_tot_ylow"]
+        if self.include_neutrals:
+            neutrals = dom[f"efd_tot_ylow"] - domp[f"efd_tot_ylow"]
+            radiation = (abs(dom["Rd+_ex"]) + abs(dom["Rd+_rec"]))*dom["dv"] * -1
+            radiation[-1] = 0 # Domain has one guard cell where volumetric quantities are zero
+            
+        ## Sources
+        source = 3/2 * (dom["Pd+_src"] + dom["Pe_src"]) * dom["dv"]
+        sheath = abs((dom["Ee_sheath"] + dom["Ed+_sheath"]) * dom["dv"])
+
+
+        ## Kinetic energy flow
+        ds_m = self.ds.shift(pos = 1)   # index minux
+        ds_p = self.ds.shift(pos = -1)   # index plus
+        ds = self.ds
+        
+        def get_ke_flow(ds):
+            return 0.5 * ds["Ne"] * 2*constants("mass_p") * ds["Ve"]**2 * abs(ds["Ve"]) * ds["dv"]
+        
+        ke_ylow = (get_ke_flow(ds_m) + get_ke_flow(ds)) / 2
+        ke_yup = (get_ke_flow(ds_p) + get_ke_flow(ds)) / 2
+        
+        print(len(ke_ylow), len(ke_yup), len(x), ds.dims)
+        
+        kinetic = (ke_ylow - ke_yup).isel(pos = slice(2,-1))
+
+        ## Total
+        total = electrons + ions + source + sheath + kinetic
+        if self.include_neutrals:
+            total += neutrals + radiation
+
+        style = dict(marker = "o", ms = 2, lw = 0.75)
+        ax.plot(x, electrons,  c = "b", label = "Electron convection", **style)
+        ax.plot(x, ions,  c = "r", label = "Ion convection", **style)
+        ax.plot(x, kinetic,  c = "purple", label = "Ion KE", **style)
+        ax.plot(x, source,  label = "Sources integral", **style)
+        ax.plot(x, sheath,  label = "Sheath", **style)
+
+        if self.include_neutrals:
+            ax.plot(x, neutrals,  c = "grey", label = "Neutral convection", **style)
+            ax.plot(x, radiation,  c = "deeppink", label = "Radiation integral", **style)
+
+        ax.plot(x, total,  c = "black", label = "Imbalance", lw = 5, alpha = 0.2)
+        ax.plot()
+
+        ax.legend(bbox_to_anchor = (1, 1), loc = "upper left")
+        ax.set_xscale("log")
+        ax.set_xlabel("Distance from target [m]")
+        ax.set_yscale("symlog", linthresh = 1)
+        ax.set_title("Total flux balance - add Qei")
+        ax.set_ylabel("Power [W]")
+        ax.grid()
