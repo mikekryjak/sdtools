6ff2a2958a3d4ec091276edfabb5c5fbbe4ccca5
Mike Kryjak <mikekryjak@gmail.com>
2025-08-12 12:17:11 +0100

    Add ddt plot to cmonitor

--- cli/cmonitor.py
+++ cli/cmonitor.py
@@ -10,6 +10,7 @@ from boutdata.collect import create_cache
 import argparse
 import os
 import time as tm
+from datetime import datetime
 
 
 
@@ -36,16 +37,24 @@ def cmonitor(path, save = False, plot = False, table = True, neutrals = False):
         casename = os.path.basename(path)
     print(f"Reading {casename}")
     print("Calculating...", end = "")
-    
+
     # Reading with cache for extra speed
     cache = create_cache(path, "BOUT.dmp")
     print("..cache", end="")
-    def get_var(name):
+    def get_var(name, guards = True):
+            
+        if guards:
+            yguards = "include_upper"
+            xguards = True
+        else:
+            yguards = False
+            xguards = False
+        
         return  boutdata.collect(
             name,
             path = path,
-            yguards = "include_upper",  # Akways with guards to minimise mistakes
-            xguards = True,  # Always with guards to minimise mistakes
+            yguards = yguards,  # Akways with guards to minimise mistakes
+            xguards = xguards,  # Always with guards to minimise mistakes
             strict = True,   # To prevent reading wrong variable by accident
             info = False,
             datafile_cache = cache
@@ -63,40 +72,42 @@ def cmonitor(path, save = False, plot = False, table = True, neutrals = False):
     dy = get_var("dy")
     dz = get_var("dz")
     J = get_var("J")
-    
-    
-    
+    dv = dx * dy * dz * J
+
+    dv_noguards = get_var("dx", guards = False) * get_var("dy", guards = False) * get_var("dz", guards = False) * get_var("J", guards = False)
+
+
+
     # Get process parameters
     t = get_var("t") * (1/Omega_ci) * 1000
-    
+
     # Sometimes the last timestep is bugged and like 1e20 higher, not sure why
     if t[-1] > t[-2]*1000:
-        skip = -2
+        skip = slice(None,-2)
         print("\nLast timestep is bugged, skipping last timestep")
     else:
-        skip = -1
-    
+        skip = slice(None,None)
+
     Ne = get_var("Ne") * Nnorm
     Nn = get_var("Nd") * Nnorm
     Te = get_var("Te") * Tnorm
     Tn = get_var("Td") * Tnorm
-    
+
     res = {}
-    
-    for param in ["ddtPe","ddtPi", "ddtPn", "ddtNe", "ddtNn", "ddtNVi", "ddtNVd"]:
+    ddt_params = ["ddt(Pe)","ddt(Pd+)", "ddt(Pd)", "ddt(Ne)", "ddt(Nn)", "ddt(NVd+)", "ddt(NVd)"]
+
+    for param in ddt_params:
         try:
-            res[param] = get_var(param)
+            res[param] = get_var(param, guards = False)
         except:
-            res[param] = np.zeros_like(Ne)
-    
+            res[param] = np.zeros_like(get_var("Ne", guards = False))
+
 
     dv = dx * dy * dz * J
 
-    
-
     # Get solver parameters
     wtime = get_var("wtime")
-    
+
     cvode = True
     try:
         nliters = get_var("cvode_nliters")
@@ -109,8 +120,8 @@ def cmonitor(path, save = False, plot = False, table = True, neutrals = False):
         nniters = np.zeros_like(t)
         nfails = np.zeros_like(t)
         lorder = np.zeros_like(t)
-    
-    
+
+
     print("..data", end="")
 
     # Calculate locations
@@ -137,73 +148,99 @@ def cmonitor(path, save = False, plot = False, table = True, neutrals = False):
     # Second row of plots
     stime = np.diff(t, prepend = t[0]*0.99)
     ms_per_24hrs = (stime) / (wtime/(60*60*24))  # ms simulated per 24 hours
-    
+
     if cvode:
         lratio = np.diff(nliters, prepend=nliters[1]*0.99) / np.diff(nniters, prepend=nniters[1]*0.99)   # Ratio of linear to nolinear iterations
         fails = np.diff(nfails, prepend = nfails[1]*0.99)
         fails[0] = fails[1]
         lorder[0] = lorder[1]
     ms_per_24hrs[0] = ms_per_24hrs[1]
-    
+
     # ddt
-    res = {}
     for param in res:
-        res[param] = (res[param] * dv) / np.sum(dv)   # Volume weighted
+
+        res[param] = (res[param] * dv_noguards[None, :, :]) / np.sum(dv_noguards)   # Volume weighted
         res[param] = np.sqrt(np.mean(res[param]**2, axis = (1,2)))  # RMS
         res[param] = np.convolve(res[param], np.ones(1), "same")    # Moving average with window of 1
-    
+
     print("..calculations", end="")
     
     # Plotting
     if plot is True or save is True:
-        scale = 1.2
-        figsize = (8*scale,4*scale)
+        scale = 1.3
+        figsize = (8*scale,5*scale)
         dpi = 150/scale
-        fig, axes = plt.subplots(2,4, figsize=figsize, dpi = dpi)
+        title_font_size = "medium"
+        # fig, axes = plt.subplots(2,4, figsize=figsize, dpi = dpi)
+        
+        # Use GridSpec for flexible layout
+        fig = plt.figure(figsize=figsize, dpi=dpi)
+        gs = fig.add_gridspec(3, 4)  # 3 rows, 4 columns
+        
 
-        fig.subplots_adjust(hspace=0.4, top = 0.85)
-        fig.suptitle(casename, y = 1.02)
+        # Original two rows of 4
+        axes = np.empty((2, 4), dtype=object)
+        for r in range(2):
+            for c in range(4):
+                axes[r, c] = fig.add_subplot(gs[r, c])
+                
+        big_ax = fig.add_subplot(gs[2, slice(None,3)])  
+
+        fig.subplots_adjust(wspace = 0.3, hspace=0.5, top = 0.85)
+        
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        fig.suptitle(f"{now} - Casename: {casename}", y = 0.93)
 
         lw = 2
-        axes[0,0].plot(t[:skip], Ne_sep[:skip], c = "darkorange", lw = lw)
-        axes[0,0].set_title("$N_{e}^{omp,sep}$")
+        axes[0,0].plot(t[skip], Ne_sep[skip], c = "darkorange", lw = lw)
+        axes[0,0].set_title("$N_{e}^{omp,sep}$", fontsize = title_font_size)
         
+        ## Physics
         if neutrals is True:
             
-            axes[0,3].plot(t[:skip], Tn_sol[:skip], c = "limegreen", lw = lw)
-            axes[0,3].set_title("$T_{n}^{omp,sol}$")
+            axes[0,3].plot(t[skip], Tn_sol[skip], c = "limegreen", lw = lw)
+            axes[0,3].set_title("$T_{n}^{omp,sol}$", fontsize = title_font_size)
             
-            axes[0,1].plot(t[:skip], Tn_core_avg[:skip], c = "darkorchid", lw = lw)
-            axes[0,1].set_title("$T_{n}^{core,avg}$")
+            axes[0,1].plot(t[skip], Tn_core_avg[skip], c = "darkorchid", lw = lw)
+            axes[0,1].set_title("$T_{n}^{core,avg}$", fontsize = title_font_size)
 
-            axes[0,2].plot(t[:skip], Nn_target[:skip], c = "deeppink", lw = lw)
-            axes[0,2].set_title("$N_{n}^{targ,max}$")
+            axes[0,2].plot(t[skip], Nn_target[skip], c = "deeppink", lw = lw)
+            axes[0,2].set_title("$N_{n}^{targ,max}$", fontsize = title_font_size)
         
         else:
             
-            axes[0,1].plot(t[:skip], Te_sol[:skip], c = "limegreen", lw = lw)
-            axes[0,1].set_title("$T_{e}^{omp,sol}$")
+            axes[0,1].plot(t[skip], Te_sol[skip], c = "limegreen", lw = lw)
+            axes[0,1].set_title("$T_{e}^{omp,sol}$", fontsize = title_font_size)
             
-            axes[0,2].plot(t[:skip], Ne_target[:skip], c = "deeppink", lw = lw)
-            axes[0,2].set_title("$N_{e}^{targ,max}$")
+            axes[0,2].plot(t[skip], Ne_target[skip], c = "deeppink", lw = lw)
+            axes[0,2].set_title("$N_{e}^{targ,max}$", fontsize = title_font_size)
             
-            axes[0,3].plot(t[:skip], Te_target[:skip], c = "darkorchid", lw = lw)
-            axes[0,3].set_title("$T_{e}^{targ,max}$")
-            
-            
+            axes[0,3].plot(t[skip], Te_target[skip], c = "darkorchid", lw = lw)
+            axes[0,3].set_title("$T_{e}^{targ,max}$", fontsize = title_font_size)
             
             
-        axes[1,0].plot(t[:skip], ms_per_24hrs[:skip], c = "k", lw = lw)
-        axes[1,0].set_title("ms $t_{sim}$ / 24hr $t_{wall}$")
+            
+        ## Speed
+        axes[1,0].plot(t[skip], ms_per_24hrs[skip], c = "k", lw = lw)
+        axes[1,0].set_title("ms $t_{sim}$ / 24hr $t_{wall}$", fontsize = title_font_size)
         
+        # #CVODE
         if cvode:
-            axes[1,1].plot(t[:skip], lratio[:skip], c = "k", lw = lw)
-            axes[1,1].set_title("linear/nonlinear")
-            axes[1,2].plot(t[:skip], np.clip(fails, 0, np.max(fails))[:skip], c = "k", lw = lw)
-            axes[1,2].set_title("nfails")
+            axes[1,1].plot(t[skip], lratio[skip], c = "k", lw = lw)
+            axes[1,1].set_title("linear/nonlinear", fontsize = title_font_size)
+            axes[1,2].plot(t[skip], np.clip(fails, 0, np.max(fails))[skip], c = "k", lw = lw)
+            axes[1,2].set_title("nfails", fontsize = title_font_size)
             axes[1,2].set_ylim(0,None)
-            axes[1,3].plot(t[:skip], lorder[:skip], c = "k", lw = lw)
-            axes[1,3].set_title("order")
+            axes[1,3].plot(t[skip], lorder[skip], c = "k", lw = lw)
+            axes[1,3].set_title("order", fontsize = title_font_size)
+            
+        ## ddts
+        for param in res:
+            big_ax.plot(t[skip], res[param][skip], label=param)
+            big_ax.set_yscale("log")
+            big_ax.grid()
+            big_ax.set_title("Volume weighted RMS ddt()", fontsize = "x-small")
+            big_ax.legend(fontsize = "x-small", loc = "upper left", bbox_to_anchor=(1,1), ncols = 2)
 
         for i in [0,1]:
             for ax in axes[i,:]:
@@ -214,7 +251,7 @@ def cmonitor(path, save = False, plot = False, table = True, neutrals = False):
                 ax.tick_params(axis='x',labelsize=8)
                 ax.tick_params(axis='y',labelsize=8)
                 
-        fig.tight_layout()
+        # fig.tight_layout()
         
         print("..figures", end="")
         
