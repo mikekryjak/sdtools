89308b945ce7e06c6745238344ac48382b254595
Mike Kryjak <mikekryjak@gmail.com>
2025-09-16 12:00:05 +0100

    Update PETSc compile script

--- install_scripts/pcompile.sh
+++ install_scripts/pcompile.sh
@@ -1,13 +1,36 @@
-# wget https://web.cels.anl.gov/projects/petsc/download/release-snapshots/petsc-3.22.1.tar.gz
-# tar -xf petsc-3.22.1.tar.gz
-# cd petsc-3.22.1/
-# ./configure --with-mpi=yes --download-hypre --download-make --with-fortran-bindings=0 --with-debugging=0 --download-fblaslapack=1
-# make PETSC_DIR=/home/mike/work/petsc-3.22.1 PETSC_ARCH=arch-linux-c-opt all -j 32
+# The script will perform a full clean install of PETSc
+# Make sure you edit the number of cores in the with-make-np flag if needed!
 
+PETSC_VERSION="3.23.3"
 
-wget https://web.cels.anl.gov/projects/petsc/download/release-snapshots/petsc-3.23.3.tar.gz
-tar -xf petsc-3.23.3.tar.gz
-mkdir petsc-3.23.3
-cd petsc-3.23.3/
-./configure --with-mpi=yes --download-hypre --download-make --with-fortran-bindings=0 --with-debugging=0 --download-fblaslapack=1
-make PETSC_DIR=/home/mike/work/petsc-3.23.3 PETSC_ARCH=arch-linux-c-opt all -j 32
+rm -rf "petsc-$PETSC_VERSION.tar.gz"
+wget "https://web.cels.anl.gov/projects/petsc/download/release-snapshots/petsc-$PETSC_VERSION.tar.gz"
+rm -rf "petsc-$PETSC_VERSION"
+tar -xf "petsc-$PETSC_VERSION.tar.gz"
+cd "petsc-$PETSC_VERSION"
+./configure \
+    COPTFLAGS="-O3" \
+    CXXOPTFLAGS="-O3" \
+    FOPTFLAGS="-O3"\
+    --with-fortran-bindings=0 \
+    --with-debugging=0 \
+    --with-mpi=yes \
+    --download-hypre \
+    --download-make \
+    --download-openblas=1 \
+    --download-metis \
+    --download-parmetis \
+    --download-zfp \
+    --download-strumpack \
+    --download-scalapack \
+    --download-ptscotch \
+    --download-mumps \
+    --download-superlu \
+    --download-suitesparse \
+    --download-superlu_dist \
+    --download-slepc \
+    --download-hpddm \
+    --with-make-np=32    # Edit if needed
+
+make PETSC_DIR=$PWD PETSC_ARCH=arch-linux-c-opt all
+
